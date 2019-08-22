6f898eb1dd48a9906d39603864d1d7d8e0e63ea4
majbc1999 <majbc1999@gmail.com>
2019-08-22 23:01:57 +0200

    Dodal slovar o vsebini knjig.

---
+++ model.py
@@ -0,0 +1,11 @@
+# Najprej dodamo slovar vseh knjig, o katerih bomo lahko glasovali in komentirali.
+
+slovar = {
+    'Harry Potter in kamen modrosti':['J. K. Rowling', 1997, 'Prva knjiga v zbriki o mladem čarovniku, ki je obnorela svet.'],
+    'Zločin in kazen':['F. M. Dostojevski', 1866, 'Psihološki roman, ki obravnava predvsem subjektivna razmišljanja o življenju, zlasti pa o upravičenosti zločina.'],
+    'Hobit ali Tja in spet nazaj':['J.R.R. Tolkien', 1937, 'Fantazijski roman kot predhodnik Gospodarja prstanov.'],
+    'Umor na Orient ekspresu':['A. Christie', 1934, 'Eno najboljših del kraljice kriminalk, nova dogodivščina Hercula Poirota.'],
+    'Grimmove pravljice':['J. in W. Grimm', 1812, 'Zbirka najzabavnejših pravljic, ki vas potopi v svet domišljije.'],
+    'Ostržek':['C.Collodi', 1881, 'Klasika o dogodivščinah lesenega fantka.'],
+    'Da Vincijeva koda':['D. Brown', 2003, 'Misteriozna zgodovinsko obarvana pustolovska drama, ki ne razočara.']
+}
