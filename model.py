bae81ba4848298d465278be83b2344ba95727290
majbc1999 <majbc1999@gmail.com>
2019-08-27 23:39:33 +0200

    Korenito spremenil model in vmesnik.

--- model.py
+++ model.py
@@ -1,44 +1,6 @@
 # Najprej dodamo slovarje vseh knjig in seznam le-teh, o katerih bomo lahko glasovali in komentirali. Ta slovar bomo lahko vnaprej še spreminjali.
 
-seznam_knjig = [
-    'Harry Potter in kamen modrosti', 
-    'Zločin in kazen', 
-    'Hobit ali Tja in spet nazaj', 
-    'Umor na Orient ekspresu', 
-    'Grimmove pravljice', 
-    'Ostržek',
-    'Da Vincijeva koda'
-]
-
-slovar_avtorjev = {
-    'Harry Potter in kamen modrosti': 'J. K. Rowling',
-    'Zločin in kazen': 'F. M. Dostojevski',
-    'Hobit ali Tja in spet nazaj': 'J.R.R. Tolkien',
-    'Umor na Orient ekspresu': 'A. Christie',
-    'Grimmove pravljice': 'J. in W. Grimm',
-    'Ostržek': 'C.Collodi',
-    'Da Vincijeva koda': 'D. Brown'
-}
-
-slovar_letnic = {
-    'Harry Potter in kamen modrosti': '1997',
-    'Zločin in kazen': '1866', 
-    'Umor na Orient ekspresu': '1934',
-    'Grimmove pravljice': '1812',
-    'Ostržek': '1881',
-    'Da Vincijeva koda': '2003'
-}
-
-slovar_opisov = {
-    'Harry Potter in kamen modrosti': 'Prva knjiga v zbriki o mladem čarovniku, ki je obnorela svet.',
-    'Zločin in kazen': 'Psihološki roman, ki obravnava predvsem subjektivna razmišljanja o življenju, zlasti pa o upravičenosti zločina.',
-    'Hobit ali Tja in spet nazaj': 'Fantazijski roman kot predhodnik Gospodarja prstanov.',
-    'Umor na Orient ekspresu': 'Eno najboljših del kraljice kriminalk, nova dogodivščina Hercula Poirota.',
-    'Grimmove pravljice': 'Zbirka najzabavnejših pravljic, ki vas potopi v svet domišljije.',
-    'Ostržek': 'Klasika o dogodivščinah lesenega fantka.',
-    'Da Vincijeva koda': 'Misteriozna zgodovinsko obarvana pustolovska drama, ki ne razočara.'
-}
-
+seznam_knjig = []
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Tukaj so pomožne funkcije za lepši izgled programa.
 
@@ -59,17 +21,10 @@ class Knjiznica:
         self.seznam_knjig = seznam_knjig
 
     def dodaj_knjigo(self, knjiga):
-        seznam_knjig.append(knjiga.naslov())
-        slovar_avtorjev[knjiga.naslov()] = knjiga.avtor()
-        slovar_letnic[knjiga.naslov()] = knjiga.leto_izdaje()
-        slovar_opisov[knjiga.naslov()] = knjiga.opis()
+        seznam_knjig.append(knjiga)
     
-    def izbrisi_knjigo(self, naslov):
-        seznam_knjig.remove(naslov)
-        del slovar_avtorjev[naslov]
-        del slovar_letnic[naslov]
-        del slovar_opisov[naslov]
-
+    def izbrisi_knjigo(self, knjiga):
+        seznam_knjig.remove(knjiga)
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Definiramo razred "Knjiga".
 
@@ -94,3 +49,9 @@ class Knjiga:
     
     def izpisi_oceno(self):
         return self.povprecna_ocena
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# V seznam knjig dodamo nekaj knjig za lažji začetek.
+
+Harry_Potter_in_kamen_modrosti = Knjiga('Harry Potter in kamen modrosti', 'J. K. Rowling', '1997', 'Prva knjiga v zbriki o mladem čarovniku, ki je obnorela svet.')
+seznam_knjig.append(Harry_Potter_in_kamen_modrosti)
--- tekstovni-vmesnik.py
+++ tekstovni-vmesnik.py
@@ -1,4 +1,4 @@
-from model import Knjiznica, Knjiga, seznam_knjig, slovar_avtorjev, slovar_letnic, slovar_opisov, povprecje
+from model import Knjiznica, Knjiga, seznam_knjig, povprecje
 
 knjiznica = Knjiznica()
 
@@ -9,41 +9,51 @@ def izberi_dejanje():
     print('Napiši številko pred dejanjem, ki ga hočeš izvesti')
     print('1) izberi knjigo')
     print('2) dodaj novo knjigo')
-    dejanje = imput('>')
+    print('3) izbriši knjigo')
+    dejanje = input('>')
     return dejanje
 
 def dodaj_novo_knjigo():
-    naslov = imput('naslov >')
-    avtor = imput('avtor >')
-    leto_izdaje = imput('leto izdaje >')
-    opis = imput('opis >')
+    naslov = input('naslov >')
+    avtor = input('avtor >')
+    leto_izdaje = input('leto izdaje >')
+    opis = input('opis >')
     knjiga = Knjiga(naslov, avtor, leto_izdaje, opis)
-    Knjiznica.dodaj_knjigo(knjiga)
+    knjiznica.dodaj_knjigo(knjiga)
     print('Uspešno ste dodali knjigo!')
 
 def izberi_knjigo():
     print('Za ogled opisa knjige, avtorja ter leta izdaje napiši NATANČEN naslov knjige.')
     for knjiga in seznam_knjig:
-        print(knjiga + ' ' + '(' + knjiga.izpisi_oceno() + '/ 5')
-    izbira = imput('>')
+        print(knjiga.naslov() + ' ' + '(' + knjiga.izpisi_oceno() + '/ 5')
+    izbira = input('>')
     return izbira
 
 def odpri_podatke_o_knjigi(knjiga):
     if knjiga in seznam_knjig:
-        print('NASLOV:'+ str(knjiga) + ' ' + '(' + knjiga.izpisi_oceno() + '/ 5')
-        print('AVTOR:' slovar_avtorjev[knjiga])
-        print('LETO IZDAJE:' slovar_letnic[knjiga])
-        print('OPIS:' slovar_opisov[knjiga])
+        print('NASLOV:'+ knjiga.naslov() + ' ' + '(' + knjiga.izpisi_oceno() + '/ 5')
+        print('AVTOR:' + knjiga.avtor())
+        print('LETO IZDAJE:' + knjiga.leto_izdaje())
+        print('OPIS:' + knjiga.opis())
         print('.................................')
         print('komentarji')
         for x in knjiga.izpisi_komentarje():
             print('- ' + str(x))
     else:
         print('Ups, te knjige ni v tvoji knjižnici. Poskusi znova')
-        return izberi_knjigo
+        return izberi_knjigo()
 
-
-
+def pozeni_knjiznico():
+    pozdrav()
+    if izberi_dejanje() == '1':
+        izberi_knjigo()
+        odpri_podatke_o_knjigi(izberi_knjigo())
+    if izberi_dejanje() == '2':
+        pass
+    if izberi_dejanje() == '3':
+        pass
+    else:
+        print('Napačna izbira.')
 
 
 
