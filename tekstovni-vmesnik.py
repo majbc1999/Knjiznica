42e26d83e413a74756d1cc9653b38e3154d51d19
majbc1999 <majbc1999@gmail.com>
2019-08-28 16:30:17 +0200

    Tekstovni vmesnik zdaj dela za prvo možnost.

--- tekstovni-vmesnik.py
+++ tekstovni-vmesnik.py
@@ -1,18 +1,36 @@
 # Najprej uvozimo vse potrebno v tekstovni vmesnik.
 from model import Knjiznica, Knjiga, povprecje
 
-# Dodamo nekaj knjig, da nebo knjižnica na začetku dolgočasna.
+# Dodamo nekaj knjig, da ne bo knjižnica na začetku dolgočasna.
 
 Harry_Potter_in_kamen_modrosti = Knjiga('Harry Potter in kamen modrosti', 'J. K. Rowling', '1997', 'Prva knjiga v zbriki o mladem čarovniku, ki je obnorela svet.')
+Zlocin_in_kazen = Knjiga('Zločin in kazen', 'F. M. Dostojevski', '1866', 'Psihološki roman, ki obravnava predvsem subjektivna razmišljanja o življenju, zlasti pa o upravičenosti zločina.')
+Hobit = Knjiga('Hobit ali Tja in spet nazaj', 'J.R.R. Tolkien', '1937', 'Fantazijski roman kot predhodnik Gospodarja prstanov.')
+Umor_na_orient_ekspresu = Knjiga('Umor na Orient ekspresu', 'A. Christie', '1934', 'Eno najboljših del kraljice kriminalk, nova dogodivščina Hercula Poirota.')
+Grimmove_pravljice = Knjiga('Grimmove pravljice', 'J. in W. Grimm', '1812', 'Zbirka najzabavnejših pravljic, ki vas potopi v svet domišljije.')
+Ostrzek = Knjiga('Ostržek','C.Collodi', '1881', 'Klasika o dogodivščinah lesenega fantka.')
+Da_Vincijeva_koda = Knjiga('Da Vincijeva koda', 'D. Brown', '2003', 'Misteriozna zgodovinsko obarvana pustolovska drama, ki ne razočara.')
 
 # Slovar naziva bo pomemben, da s pomočjo naslova knjige dobimo naziv le-te.
 slovar_naziva = {
-    'Harry Potter in kamen modrosti': Harry_Potter_in_kamen_modrosti
+    'Harry Potter in kamen modrosti': Harry_Potter_in_kamen_modrosti,
+    'Hobit ali Tja in spet nazaj': Hobit,
+    'Zločin in kazen': Zlocin_in_kazen,
+    'Umor na Orient ekspresu': Umor_na_orient_ekspresu,
+    'Grimmove pravljice': Grimmove_pravljice,
+    'Ostržek': Ostrzek,
+    'Da Vincijeva koda': Da_Vincijeva_koda
 }
 
 # Ustvarimo našo začetno knjižnico.
 knjiznica = Knjiznica()
 knjiznica.dodaj_knjigo(Harry_Potter_in_kamen_modrosti)
+knjiznica.dodaj_knjigo(Hobit)
+knjiznica.dodaj_knjigo(Zlocin_in_kazen)
+knjiznica.dodaj_knjigo(Umor_na_orient_ekspresu)
+knjiznica.dodaj_knjigo(Grimmove_pravljice)
+knjiznica.dodaj_knjigo(Ostrzek)
+knjiznica.dodaj_knjigo(Da_Vincijeva_koda)
 
 # Tukaj so vse funkcije, ki jih bomo potrebovali
 def pozdrav():
@@ -74,8 +92,6 @@ def odpri_podatke_o_knjigi(knjiga):
                 print('     - ' + str(x))
         if ali_bomo_dodali_nov_komentar():
             besedilo_komentarja(knjiga)
-        elif not ali_bomo_dodali_nov_komentar():
-            pass
     else:
         print('Ups, te knjige ni v tvoji knjižnici. Poskusi znova')
         return izberi_knjigo()
@@ -83,17 +99,18 @@ def odpri_podatke_o_knjigi(knjiga):
 
 # To je funkcija, ki bo zagnala vse ostale funkcije.
 def pozeni_knjiznico():
-    pozdrav()
-    if izberi_dejanje() == '1':
-        odpri_podatke_o_knjigi(izberi_knjigo())
-    if izberi_dejanje() == '2':
-        pass
-    if izberi_dejanje() == '3':
-        pass
-    if izberi_dejanje() == '4':
-        pass
-    else:
-        print('Napačna izbira.')
+    while True:
+        pozdrav()
+        if izberi_dejanje() == '1':
+            odpri_podatke_o_knjigi(izberi_knjigo())
+        elif izberi_dejanje() == '2':
+            pass
+        elif izberi_dejanje() == '3':
+            pass
+        elif izberi_dejanje() == '4':
+            pass
+        else:
+            print('Napačna izbira.')
 
 # Knjižnico še poženemo
 pozeni_knjiznico()
